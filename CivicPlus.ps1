a77468f6cc0d13fb7ddd43ead97300bf16cdee41
Avd6977 <avd6977@gmail.com>
2019-11-19 08:07:33 -0600

    Update VS Build Tools to 2019

--- CivicPlus.ps1
+++ CivicPlus.ps1
@@ -38,8 +38,8 @@ code --install-extension msjsdiag.debugger-for-edge
 #--- Tools ---
 choco install -y nodejs-lts # Node.js LTS, Recommended for most users
 # choco install -y nodejs # Node.js Current, Latest features
-choco install -y visualstudio2017buildtools
-choco install -y visualstudio2017-workload-vctools
+choco install -y visualstudio2019buildtools
+choco install -y visualstudio2019-workload-vctools
 choco install -y python2 # Node.js requires Python 2 to build native modules
 
 Enable-UAC
